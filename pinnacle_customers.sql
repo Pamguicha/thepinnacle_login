b1fea0abd7c239e7272ab4981609133fc7c31442
Yourfirstname Yourlastname <your-email@example.com>
2024-11-21 12:12:21 +1100

    updated sql with updated edit store procedure

--- pinnacle_customers.sql
+++ pinnacle_customers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Nov 21, 2024 at 01:00 AM
+-- Generation Time: Nov 21, 2024 at 01:11 AM
 -- Server version: 5.7.39
 -- PHP Version: 7.4.33
 
@@ -32,6 +32,7 @@ END$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `editData` (IN `p_fullName` VARCHAR(255), IN `p_type_beer` VARCHAR(255), IN `p_amount` VARCHAR(255), IN `p_pickup_day` VARCHAR(255), IN `p_pickup_time` VARCHAR(255), IN `p_id_orders` INT)   BEGIN
   UPDATE orderBeers
   SET
+    fullName = p_fullName,   -- Update the fullName
     type_beer = p_type_beer,
     amount = p_amount,
     pickup_day = p_pickup_day,
