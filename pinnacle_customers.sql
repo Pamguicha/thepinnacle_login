195c6d19c521a99373862235361b5377962b85cb
Yourfirstname Yourlastname <your-email@example.com>
2024-11-22 11:39:38 +1100

    Updated SQL file

--- pinnacle_customers.sql
+++ pinnacle_customers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Nov 21, 2024 at 01:11 AM
+-- Generation Time: Nov 22, 2024 at 12:38 AM
 -- Server version: 5.7.39
 -- PHP Version: 7.4.33
 
@@ -25,6 +25,11 @@ DELIMITER $$
 --
 -- Procedures
 --
+CREATE DEFINER=`root`@`localhost` PROCEDURE `deleteOrder` (IN `p_id_orders` INT)   BEGIN
+    DELETE FROM `orderBeers`
+    WHERE id_orders = p_id_orders;
+END$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `displayData` (IN `p_ID_customer` INT)   BEGIN
     SELECT * FROM orderBeers WHERE ID_customer = p_ID_customer;
 END$$
@@ -96,10 +101,13 @@ CREATE TABLE `orderBeers` (
 --
 
 INSERT INTO `orderBeers` (`ID_customer`, `fullName`, `type_beer`, `amount`, `pickup_day`, `pickup_time`, `id_orders`) VALUES
-(3, 'Pamela Mardones', 'Amber', '32', 'Thursday', '3 pm', 2),
 (2, 'Sean Murphy', '34 beer', '30 pack', 'friday', '3 ppm', 3),
-(1, 'Tom Richard', 'stout', '30 beers', 'monday', '4 pm', 4),
-(4, 'Pablo Marmol', 'Amber', '30', 'friday', '3 pm', 7);
+(4, 'Pablo Picapiedra', 'Amber', '30', 'wednesday', '2 pm', 8),
+(1, 'Tomas Moises Quiroga', 'Stout', '32', 'friday', '4 30 pm', 11),
+(1, 'Tom oryan', 'Amber', '32', 'thursday', '2 pm', 12),
+(3, 'Pamela Belaunde', 'Amber', '32', 'friday', '3 30 pm', 13),
+(3, 'Pamela Mardones G', 'Amber', '42', 'friday', '3 pm', 14),
+(1, 'Tom Mardones', 'Amber', '30', 'friday', '4 pm', 16);
 
 --
 -- Indexes for dumped tables
@@ -132,7 +140,7 @@ ALTER TABLE `loginData`
 -- AUTO_INCREMENT for table `orderBeers`
 --
 ALTER TABLE `orderBeers`
-  MODIFY `id_orders` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_orders` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
